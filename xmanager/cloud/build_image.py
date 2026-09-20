ab32a8903249f2a686393d21c3e94be97ebe1c33
Kristian Hartikainen <kristian@ekarobotics.com>
2026-09-20 13:15:44 +0000

    Quote bare `$@` in container entrypoints (#77)

--- xmanager/cloud/build_image.py
+++ xmanager/cloud/build_image.py
@@ -15,6 +15,7 @@
 
 import json
 import os
+import re
 import shutil
 import tempfile
 from typing import Dict, List, Optional
@@ -61,6 +62,8 @@ fi
 
 {cmds}
 """
+
+_BARE_ARGS_SUFFIX = re.compile(r'\$@$', re.MULTILINE)
 
 
 def build(
@@ -278,7 +281,9 @@ def _get_entrypoint_commands(py_executable: xm.PythonContainer) -> str:
     )
   cmds = '\n'.join(cmds)
   # Allow passing extra parameters to the commands.
-  if not cmds.endswith(('$@', '"$@"')):
+  if _BARE_ARGS_SUFFIX.search(cmds):
+    cmds = _BARE_ARGS_SUFFIX.sub('"$@"', cmds)
+  elif not cmds.endswith('"$@"'):
     cmds = cmds + ' "$@"'
   return cmds
 
